729af67a1ab28d838cdc6f79763d3d34cbfca7d8
devinpledwards <devinpledwards@live.com>
2022-07-28 19:17:38 +0000

    added categories and category/entry relationship

--- app/controllers/entries_controller.rb
+++ app/controllers/entries_controller.rb
@@ -65,6 +65,6 @@ class EntriesController < ApplicationController
 
     # Only allow a list of trusted parameters through.
     def entry_params
-      params.require(:entry).permit(:meal_type, :calories, :protein, :carbs, :fats)
+      params.require(:entry).permit(:meal_type, :calories, :protein, :carbs, :fats, :category_id)
     end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_07_25_230838) do
+ActiveRecord::Schema.define(version: 2022_07_28_180409) do
+
+  create_table "categories", force: :cascade do |t|
+    t.string "title"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "entries", force: :cascade do |t|
     t.string "meal_type"
@@ -20,6 +26,8 @@ ActiveRecord::Schema.define(version: 2022_07_25_230838) do
     t.integer "fats"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "category_id"
+    t.index ["category_id"], name: "index_entries_on_category_id"
   end
 
 end
